d69dfa097771a639fd7526a570630f11c4b8ae6a
Karim <karim.ayoub.job@gmail.com>
2025-05-12 14:46:51 +0200

    Fin de l'activités 4

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import Category from "./pages/Category";
 import Layout from "./components/Layout";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
+        <Route index element={<Home />} />
         <Route path="/category/:id" element={<Category />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,5 @@
+function NotFound() {
+    return <h1>Page non trouvée</h1>;
+}
+
+export default NotFound;
